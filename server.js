3ed25a271d43f0f3da510c4ecfea93313642c70a
stefan hall <stefhall@comcast.net>
2021-06-26 13:22:21 -0700

    Adding package.json, changes to home-routes

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
+const express = require('express');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 const routes = require('./controllers/');
 
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+app.use(routes);
+
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, function() {
+        console.log('App listening on PORT ' + PORT);
+    });
+});
