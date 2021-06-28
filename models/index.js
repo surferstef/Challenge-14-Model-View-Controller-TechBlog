95a81a3c54e900299f576affe3e3ef189464a46c
stefan hall <stefhall@comcast.net>
2021-06-27 18:39:20 -0700

    Updating index for models, additionally adding vote.js in case we want that functionality

--- models/index.js
+++ models/index.js
@@ -1,4 +1,47 @@
 const User = require('./User');
 const Post = require('./Post');
+const Comment = require('./Comment');
+const Vote = require('./Vote');
 
-module.exports = { User, Post };
+
+
+// Building Associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+  });
+  
+  Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+  });
+
+  Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
+  
+  Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+  });
+  
+  User.hasMany(Comment, {
+    foreignKey: 'user_id'
+  });
+  
+  Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+  });
+
+
+
+module.exports = { User, Post, Comment, Vote }
