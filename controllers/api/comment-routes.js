7ca99105f275c4ceca62cc3e3f63149a91a0070b
stefan hall <stefhall@comcast.net>
2021-06-27 23:05:06 -0700

    Updating Comment-routes

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,22 +1,41 @@
 const router = require('express').Router();
-const { Comment } = require('../../models');
+const { Comment, User } = require('../../models');
 
 router.get('/', (req, res) => {
-
+    Comment.findAll({
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: [
+            {
+                model: User,
+                attributes: ['id', 'username', 'email', 'password'],
+            },
+        ],
+    })
+    .then(dbCommentData => {
+        res.json(dbCommentData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 router.post('/', (req, res) => {
-    Comment.create({
+    // check the session
+    if (req.session) {
+      Comment.create({
         comment_text: req.body.comment_text,
-        user_id: req.body.user_id,
-        post_id: req.body.post_id
+        post_id: req.body.post_id,
+        // use the id from the session
+        user_id: req.session.user_id
       })
         .then(dbCommentData => res.json(dbCommentData))
         .catch(err => {
           console.log(err);
           res.status(400).json(err);
         });
-});
+    }
+  });
 
 router.delete('/:id', (req, res) => {
 
