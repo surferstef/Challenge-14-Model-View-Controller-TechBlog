a7eb7da5a164884828e794f6550cdd21badf2f2b
stefan hall <stefhall@comcast.net>
2021-06-28 19:09:21 -0700

    Add changes to login.js

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,20 +1,26 @@
-async function signupFormHandler(event) {
+async function loginFormHandler(event) {
     event.preventDefault();
   
-    const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
   
-    if (username && email && password) {
-      const response = await fetch('/api/users', {
+    if (email && password) {
+      const response = await fetch('/api/users/login', {
         method: 'post',
         body: JSON.stringify({
-          username,
           email,
           password
         }),
         headers: { 'Content-Type': 'application/json' }
       });
-      console.log(response);
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert(response.statusText);
+      }
     }
   }
+  
+  document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+  
